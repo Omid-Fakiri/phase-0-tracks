61a4524ffc56032b92054c63962d1616465d2415
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-07 15:59:20 -0800

    Adds release 2 input methods

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,9 +1,14 @@
 puts "What is the hamster's name?"
+name = gets.chomp
 
 puts "How loud is the hamster from a scale of 1 to 10?"
+volume = gets.chomp.to_i
 
 puts "What is the hamster's fur color?"
+color = gets.chomp
 
 puts "Is the hamster a good candidate for adoption? (y/n)"
+adoption = gets.chomp.downcase
 
 puts "How old is the hamster?"
+age = gets.chomp
