21831330d61996838ee124bcc49553d64786ec52
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-07 15:57:25 -0800

    Adds release 1 questions.

---
+++ ruby/hamsters.rb
@@ -0,0 +1,9 @@
+puts "What is the hamster's name?"
+
+puts "How loud is the hamster from a scale of 1 to 10?"
+
+puts "What is the hamster's fur color?"
+
+puts "Is the hamster a good candidate for adoption? (y/n)"
+
+puts "How old is the hamster?"
