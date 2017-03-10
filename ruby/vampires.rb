80878ba421b19331886c62564166d37a9f7595ee
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-10 14:37:57 -0800

    This is Release 1 for Assignment 4.4

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+puts "What is your name?"
+nme = gets.chomp
+
+puts "How old are you? What year were you born?"
+age = gets.chomp
+year = gets.chomp
+
+puts "Would you care for some garlic bread?"
+bread = gets.chomp
+
+puts "Would you like to enroll in our insurance plan?"
+insurance = gets.chomp
+
