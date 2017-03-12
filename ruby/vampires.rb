dbd0edae17501a6a2f1b6e6aee8794f86ceac079
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-11 17:23:21 -0800

    Takes care of Release 2 and 3 for Assignment 4.4

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,85 @@
 puts "What is your name?"
-nme = gets.chomp
+user_name = gets.chomp.to_s
 
-puts "How old are you? What year were you born?"
-age = gets.chomp
-year = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i 
 
-puts "Would you care for some garlic bread?"
-bread = gets.chomp
+puts "What year were you born?"
+year = gets.chomp.to_i
 
-puts "Would you like to enroll in our insurance plan?"
-insurance = gets.chomp
+current_year = 2017
+verified_age = current_year - year
 
+puts "Would you care for some garlic bread? (yes or no)"
+bread = gets.chomp.downcase
+
+until bread == "yes" || bread == "no"
+	puts "Would you care for some garlic bread? (yes or no)"
+	bread = gets.chomp.downcase
+end
+
+puts "Would you like to enroll in our insurance plan? (yes or no)"
+insurance = gets.chomp.downcase
+
+until insurance == "yes" || insurance == "no"
+	puts "Would you like to enroll in our insurance plan? (yes or no)"
+	insurance = gets.chomp.downcase
+end
+
+=begin
+if (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes")
+	puts "Probably not a vampire."
+elsif (age != verified_age && age != verified_age - 1) || (bread == "no" && insurance == "yes") || (bread == "yes" && insurance == "no")
+	puts "Probably a vampire."
+elsif (age != verified_age && age != verified_age - 1) && (bread == "no" || insurance == "no")
+	puts "Almost certainly a vampire."
+elsif (user_name == "Drake Cula" || user_name == "Tu Fang")
+	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
+end
+=end
+#This takes care of probably not a vampire, probably a vampire, and results inconclusive portion
+
+=begin
+if (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes")
+	puts "Probably not a vampire."
+elsif (age != verified_age && age != verified_age - 1) && (bread == "yes" || insurance == "yes")
+	puts "Probably a vampire."
+elsif (age != verified_age && age != verified_age - 1) && (bread == "no" && insurance == "no")
+	puts "Almost certainly a vampire."
+elsif (user_name == "Drake Cula" || user_name == "Tu Fang")
+	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
+end
+=end
+#This takes care of the Almost certainly a vampire portion.
+#puts (wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine - TRUE
+#By using or, if either condition is true, the whole condition will still be false.
+
+=begin
+if (user_name != "Drake Cula" && user_name != "Tu Fang") && (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes")
+	puts "Probably not a vampire."
+elsif (user_name != "Drake Cula" && user_name != "Tu Fang") && (age != verified_age && age != verified_age - 1) && (bread == "yes" || insurance == "yes")
+	puts "Probably a vampire."
+elsif (user_name != "Drake Cula" && user_name != "Tu Fang") && (age != verified_age && age != verified_age - 1) && (bread == "no" && insurance == "no")
+	puts "Almost certainly a vampire."
+elsif (user_name == "Drake Cula" || user_name == "Tu Fang")
+	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
+end
+=end
+#This takes care of all the statements
+if (user_name != "Drake Cula" && user_name != "Tu Fang") && (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes")
+	puts "Probably not a vampire."
+elsif (user_name != "Drake Cula" && user_name != "Tu Fang") && (age != verified_age && age != verified_age - 1) && (bread == "yes" || insurance == "yes")
+	puts "Probably a vampire."
+elsif (user_name != "Drake Cula" && user_name != "Tu Fang") && (age != verified_age && age != verified_age - 1) && (bread == "no" && insurance == "no")
+	puts "Almost certainly a vampire."
+elsif (user_name == "Drake Cula" || user_name == "Tu Fang")
+	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
+end
