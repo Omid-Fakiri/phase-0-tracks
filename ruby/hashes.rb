e6999849acf93d140766a7896e85d472f4f947b8
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-15 17:43:19 -0700

    Provides psuedocode/algorithm for Release 3 in Assignment 5.2

---
+++ ruby/hashes.rb
@@ -0,0 +1,7 @@
+=begin
+This is the algorithm for the code:
+- The user will be asked to input all of the information via the gets.chomp method.
+- Symbols such as "Address:" (without the quotes) will be used in the program as input data.
+- The output will be returned as a hash.
+- The user will have an opportunity to update the client's details with an if/else statement.
+=end
