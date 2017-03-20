f05c4a8ff8ca4bbae2541253007de7c46c0599a1
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-20 14:52:29 -0700

    Provides questions for employee

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,95 +1,28 @@
 puts "What is your name?"
-user_name = gets.chomp.to_s
+user_name = gets.to_s
 
-puts "How old are you?"
-age = gets.chomp.to_i 
+puts "How old are you? (age number only)"
+age = gets.to_i
 
-puts "What year were you born?"
-year = gets.chomp.to_i
+puts "What year were you born? (year number only)"
+year = gets.to_i
 
-current_year = 2017
-verified_age = current_year - year
+current_year = Time.now.year
+verified age = current_year - year
 
-puts "Would you care for some garlic bread? (yes or no)"
+puts "We have some garlic bread in the cafeteria. Would you like some? (yes or no)"
 bread = gets.chomp.downcase
 
 until bread == "yes" || bread == "no"
-	puts "Would you care for some garlic bread? (yes or no)"
+	puts "We have some garlic bread in the cafeteria. Would you like some? (yes or no)"
 	bread = gets.chomp.downcase
 end
 
-puts "Would you like to enroll in our insurance plan? (yes or no)"
-insurance = gets.chomp.downcase
+puts "Would you like to enroll in our health insurance plan? (yes or no)"
+insurannce = gets.chomp.downcase
 
 until insurance == "yes" || insurance == "no"
-	puts "Would you like to enroll in our insurance plan? (yes or no)"
-	insurance = gets.chomp.downcase
+	puts "Would you like to enroll in our health insurance plan? (yes or no)"
+	insurannce = gets.chomp.downcase
 end
 
-puts "Please list the items you are allergic to one at a time. (say done when finished)"
-allergy = gets.chomp.downcase
-
-until allergy == "done" || allergy == "sunshine"
-	allergy = gets.chomp.downcase
-end
-
-if (user_name != "Drake Cula" && user_name != "Tu Fang") && (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes") && (allergy != "sunshine") 
-	puts "Probably not a vampire."
-elsif (allergy == "sunshine")
-	puts "Probably a vampire."
-elsif (user_name != "Drake Cula" && user_name != "Tu Fang") && (age != verified_age && age != verified_age - 1) && (bread == "no" && insurance == "no")
-	puts "Almost certainly a vampire."
-elsif (user_name == "Drake Cula" || user_name == "Tu Fang")
-	puts "Definitely a vampire."
-else
-	puts "Results inconclusive."
-end
-
-puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
-
-=begin
-if (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes")
-	puts "Probably not a vampire."
-elsif (age != verified_age && age != verified_age - 1) || (bread == "no" && insurance == "yes") || (bread == "yes" && insurance == "no")
-	puts "Probably a vampire."
-elsif (age != verified_age && age != verified_age - 1) && (bread == "no" || insurance == "no")
-	puts "Almost certainly a vampire."
-elsif (user_name == "Drake Cula" || user_name == "Tu Fang")
-	puts "Definitely a vampire."
-else
-	puts "Results inconclusive."
-end
-=end
-#This takes care of probably not a vampire, probably a vampire, and results inconclusive portion
-
-=begin
-if (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes")
-	puts "Probably not a vampire."
-elsif (age != verified_age && age != verified_age - 1) && (bread == "yes" || insurance == "yes")
-	puts "Probably a vampire."
-elsif (age != verified_age && age != verified_age - 1) && (bread == "no" && insurance == "no")
-	puts "Almost certainly a vampire."
-elsif (user_name == "Drake Cula" || user_name == "Tu Fang")
-	puts "Definitely a vampire."
-else
-	puts "Results inconclusive."
-end
-=end
-#This takes care of the Almost certainly a vampire portion.
-#puts (wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine - TRUE
-#By using or, if either condition is true, the whole condition will still be false.
-
-=begin
-if (user_name != "Drake Cula" && user_name != "Tu Fang") && (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes")
-	puts "Probably not a vampire."
-elsif (user_name != "Drake Cula" && user_name != "Tu Fang") && (age != verified_age && age != verified_age - 1) && (bread == "yes" || insurance == "yes")
-	puts "Probably a vampire."
-elsif (user_name != "Drake Cula" && user_name != "Tu Fang") && (age != verified_age && age != verified_age - 1) && (bread == "no" && insurance == "no")
-	puts "Almost certainly a vampire."
-elsif (user_name == "Drake Cula" || user_name == "Tu Fang")
-	puts "Definitely a vampire."
-else
-	puts "Results inconclusive."
-end
-=end
-#This takes care of all the statements
