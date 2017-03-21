68270db650fbbed8d7d20a8e6dcaef84110afe34
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-21 11:52:48 -0700

    Separates allergy condition from rest

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -38,31 +38,31 @@ while count < employees
 				puts "Would you like to enroll in our health insurance plan? (yes or no)"
 				insurance = gets.chomp.downcase
 			end
+	end			
 
-			puts "Please list things that you are allergic to one at a time. (Type 'done' when finished)"
+	puts "Please list things that you are allergic to one at a time. (Type 'done' when finished)"
+	allergy = gets.chomp.downcase
+
+		until allergy == "done" || allergy == "sunshine"
 			allergy = gets.chomp.downcase
+		end
 
-			until allergy == "done"
-				allergy = gets.chomp.downcase
-					break if allergy == "sunshine"
-			end
-	end
-	
-	if user_name == "Drake Cula" || user_name == "Tu Fang"
-		puts "Definitely a vampire."
-	elsif (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes")
+		if allergy == "sunshine"
+			puts "Probably a vampire."
+			count += 1
+			next
+		end
+
+	if (age == verified_age || age == verified_age - 1) && (bread == "yes" || insurance == "yes")
 		puts "Probably not a vampire."
 	elsif (age != verified_age && age != verified_age - 1) && (bread == "yes" || insurance == "yes")
-		puts "Probably a vampire."
-	elsif allergy == "sunshine"
 		puts "Probably a vampire."
 	elsif (age != verified_age && age != verified_age - 1 && bread == "no" && insurance == "no") 
 		puts "Almost certainly a vampire."
 	else
 		puts "Results inconclusive."
 	end
-
-	count += 1 
+count += 1
 end
 
 puts "Actually, nevermind! What do these questions have to do with anything? Let's all be friends."
