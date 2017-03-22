4b8045a6e7fdb38fc218f857a7f9cad2cde3c60e
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-22 15:23:32 -0700

    Adds roll_over method, along with driver code for it

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -10,12 +10,17 @@ class Puppy
   	bark.times do p "Woof!" end
   end
 
+  def roll_over
+  	puts "rolls over"
+  end
+  
 end
 
 
 puppy = Puppy.new
 puts puppy.fetch("ball")
 puts puppy.speak(5)
+puts puppy.roll_over
 
 
 
