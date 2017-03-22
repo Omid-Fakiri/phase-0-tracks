34e4f974e7f1552599810e3df098dab332917803
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-22 15:20:21 -0700

    Adds driver code for fetch method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,19 @@
+#This is for Release 0
 class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
+  
 end
+
+
+puppy = Puppy.new
+puts puppy.fetch("ball")
+
+
+
+
+
 
