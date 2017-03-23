77590d9725e213190e5118fe4cc7e2a55d586d28
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-22 18:53:31 -0700

    Adds diverse initializations to driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -22,3 +22,12 @@ end
 santa = Santa.new("gender", "ethnicity")
 puts santa.speak
 puts santa.eat_milk_and_cookies("chocolate chip cookie")
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
