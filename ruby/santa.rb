f1ecd93aa114f3ecd44963be3049976e468a3316
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-23 19:22:00 -0700

    Updates the diverse initializations in the driver code.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -24,10 +24,10 @@ puts santa.speak
 puts santa.eat_milk_and_cookies("chocolate chip cookie")
 
 santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
