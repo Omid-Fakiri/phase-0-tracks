42d272f478400eee0472508d60b6fd777bb4ff81
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-24 09:45:07 -0700

    Adds 2 getter methods, 1 setter method, and 2 attribute-changing methods.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -17,6 +17,36 @@ class Santa
 		@age = 0
 	end
 
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def celebrate_birthday(age)
+		age += 1
+	end
+
+	def get_mad_at(reindeer)
+		case reindeer
+			when "Rudolph" then @rank = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen", "Rudolph"] 		
+			when "Dasher" then @rank = ["Rudolph", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen", "Dasher"]
+			when "Dancer" then @rank = ["Rudolph", "Dasher", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen", "Dancer"]
+			when "Prancer" then @rank = ["Rudolph", "Dasher", "Dancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen", "Prancer"]
+			when "Vixen" then @rank = ["Rudolph", "Dasher", "Dancer", "Prancer", "Comet", "Cupid", "Donner", "Blitzen", "Vixen"]
+			when "Comet" then @rank = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Cupid", "Donner", "Blitzen", "Comet"]
+			when "Cupid" then @rank = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Donner", "Blitzen", "Cupid"]
+			when "Donner" then @rank = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Blitzen", "Donner"]
+			else @rank = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		end
+		puts "#{reindeer} behaved badly.  So, the current reindeer ranking is: #{@rank}"
+	end
 end
 
 santa = Santa.new("gender", "ethnicity")
@@ -27,7 +57,13 @@ santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+   santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
 
 p santas
+santa = Santa.new("male", "black")
+p santa.age
+p santa.ethnicity
+p santa.gender=("half-man, half-robot")
+p santa.get_mad_at("Rudolph")
+p santa.celebrate_birthday(6)
