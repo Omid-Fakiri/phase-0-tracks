651d4e4f407123d8159ffaaa255b809097309fb2
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-24 23:01:06 -0700

    Adds gender, ethnicity, reindeer_ranking, and age to initialization method. Adds driver code to the bottom

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -9,6 +9,26 @@ class Santa
 		puts "That was a good #{cookie}!"
 	end
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@rank = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
+	
+end
+
+santa = Santa.new("gender", "ethnicity")
+puts santa.speak
+puts santa.eat_milk_and_cookies("chocolate chip cookie")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
+
