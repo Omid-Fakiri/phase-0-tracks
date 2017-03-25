9efd0a0cf10c2011974fb3e23d643565d9426b33
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-25 00:04:34 -0700

    Comments out standalone Module, adds the mixin version.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,32 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(words)
-  	words + "!!!" + " :)"
-  end
+#   def self.yelling_happily(words)
+#   	words + "!!!" + " :)"
+#   end
+# end
+
+# puts Shout.yell_angrily("I am angry")
+# puts Shout.yelling_happily("I am happy")
+
+module Shout
+	def yell(words)
+		puts "I am so #{words}, I don't know how to !"
+	end
 end
 
-puts Shout.yell_angrily("I am angry")
-puts Shout.yelling_happily("I am happy")
+class Angry
+	include Shout
+end
+
+class Happily
+	include Shout
+end
+
+angry = Angry.new
+angry.yell("angry")
+
+happily = Happily.new
+happily.yell("happy")
