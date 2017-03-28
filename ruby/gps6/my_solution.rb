ee74bc39f23956a24148baf6183c24ec90fed6e4
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-28 15:36:03 -0700

    Adds comments for all the methods (Release 3)

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -10,20 +10,20 @@
 require_relative 'state_data'
 
 class VirusPredictor
-
+  # initialize defines each attribute
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+  # virus_effects runs and combines the private methods
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
 
   private
-
+  # predicted deaths calculates predicted deaths based on population, population_density depending on the state listed.
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -41,7 +41,7 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
+  # speed of spread will rate the speed based on population density of the given state.
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -82,7 +82,6 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
-
 
 #=======================================================================
 # Reflection Section
