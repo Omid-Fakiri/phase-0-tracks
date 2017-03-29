f0e09070b1fa08e436bb80f94a127c902ed6284f
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-29 14:38:11 -0700

    Compares Functions (aka Methods)

--- js/explore.js
+++ js/explore.js
@@ -18,4 +18,12 @@ You will get an error message if you don't do it in JavaScript.
 2) In JavaScript, you MUST have {} at the beginning and end of your conditional statements.  This is not needed in Ruby.
 3) In JavaScript, when you want to use and else if conditional, you write, "else if".  In Ruby, you write, "elsif".
 
+This is for Release 2
+1) In JavaScript, you would type "function" before declaring it.  In ruby, you would declare methods by typing "def".  Also, in JavaScript, you do NOT 
+need an "end" statement for the functions.
+2) JavaScript do not have implicit returns.  When I deleted the "return" keyword, the console printed "undefined".
+3) When I took away the parenthesis from the function, I got "SyntaxError: Unexpected token, expected" printed to the console.
+4) You won't necessarily need to parenthesis in the function call to prevent error messages.  You'll just get something like this to the output, 
+"=> [Function: sayHello]."  I just don't know how crucial this becomes for a program to be run.
+
 
