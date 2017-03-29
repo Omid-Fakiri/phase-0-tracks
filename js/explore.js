9cbd087fa793af77ec7a121e05333ada7b7ae759
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-29 15:00:01 -0700

    Compares Loops

--- js/explore.js
+++ js/explore.js
@@ -13,17 +13,34 @@ fixes.
 This is for Release 1
 Comparing if/else statements.
 3 differences from JavaScript to Ruby:
-1) In JavaScript, when you set conditional statements with variables, you MUST use parenthesis around the variables.  In Ruby, it is optional.
+1. In JavaScript, when you set conditional statements with variables, you MUST use parenthesis around the variables.  In Ruby, it is optional.
 You will get an error message if you don't do it in JavaScript.
-2) In JavaScript, you MUST have {} at the beginning and end of your conditional statements.  This is not needed in Ruby.
-3) In JavaScript, when you want to use and else if conditional, you write, "else if".  In Ruby, you write, "elsif".
+2. In JavaScript, you MUST have {} at the beginning and end of your conditional statements.  This is not needed in Ruby.
+3. In JavaScript, when you want to use and else if conditional, you write, "else if".  In Ruby, you write, "elsif".
 
 This is for Release 2
-1) In JavaScript, you would type "function" before declaring it.  In ruby, you would declare methods by typing "def".  Also, in JavaScript, you do NOT 
+1. In JavaScript, you would type "function" before declaring it.  In ruby, you would declare methods by typing "def".  Also, in JavaScript, you do NOT 
 need an "end" statement for the functions.
-2) JavaScript do not have implicit returns.  When I deleted the "return" keyword, the console printed "undefined".
-3) When I took away the parenthesis from the function, I got "SyntaxError: Unexpected token, expected" printed to the console.
-4) You won't necessarily need to parenthesis in the function call to prevent error messages.  You'll just get something like this to the output, 
+2. JavaScript do not have implicit returns.  When I deleted the "return" keyword, the console printed "undefined".
+3. When I took away the parenthesis from the function, I got "SyntaxError: Unexpected token, expected" printed to the console.
+4. You won't necessarily need to parenthesis in the function call to prevent error messages.  You'll just get something like this to the output, 
 "=> [Function: sayHello]."  I just don't know how crucial this becomes for a program to be run.
 
+This is for Release 3
+1. var i;
+
+for (i = 0; i <= 10; i++) {
+  console.log(i);
+}
+This counts from 0 to 10.
+
+2. Changing the counter to i+=2 in this case printed out the even numbers, starting from 0.
+
+3. var i;
+
+for (i = 10; i >= 0; i--) {
+  console.log(i);
+} 
+
+This counts from 10 to 0.
 
