8e4dba321b281b2402f38f408a9162a33d064820
Omid-Fakiri <ofakiri@hotmail.com>
2017-03-29 14:13:22 -0700

    Compares Variables, Data Types, and Printing

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+/*This is for Release 0
+1. The equivalent of nil in JavaScript is "null" on the input.  And, it looks like the output will be "undefined".
+2. It is a good idea to use var, simply to avoid making the variable a global one, making it susceptable to being equal to something different in 
+the program.
+As far as I know right now, in Ruby, you can use signs like @ or $, to label the variables as instance and global, respectfully.  
+Right now, I don't know if there are such options in JavaScript.
+3. In JavaScript, you use, "+ variableGoesHere" to print a variable.  You also use console.log("Print statement here") to print something.
+4. In JavaScript, you end a line of code with the ";" symbol.
+5. In JavaScript, you write single-line comments by using "//".  But, you can use "/*" to start a multi-line comment section, and end it with the reverse
+of the characters. If I do it now, I will lose the comment capability here, and the aesthetics will not be as pleasing even if I make the necesssary 
+fixes.
+
+
+
