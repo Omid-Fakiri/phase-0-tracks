a6219652e0ab5402ed6ca7a444829f343e643834
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-06 10:11:46 -0700

    Logs results ran in IRB

--- ruby/secret_agents_sea_lions.rb
+++ ruby/secret_agents_sea_lions.rb
@@ -11,3 +11,36 @@
  =end
 
 
+ =begin
+ -This is for Release 1
+ * "agent"[0] retuned "a"
+ * "agent"[1] returned "g"
+ * "agent"[1..3] returned "gen"
+ * "agent"[1...3] returned "ge"
+ * str = "abcdef"
+   str[3] returned "d"
+ * "mission"[-1] returned "n"
+ * "mission"[-2] returned "o"
+ * "agent".index("a") returned "0"
+ * "agent".index("z") returned "nil"
+ * "agent".delete("g") returned "aent"
+ * "mission".delete("s") returned "miion"
+ * "agent".delete("age") returned "nt"
+ * "agent".delete("mission") returned "aget"
+ * "a".next returned "b"
+ * "a".next.next.next returned "d"
+ * "z".next returned "aa"
+ * "secret".reverse returned "terces"
+ * "z".length returned "1"
+ * "FBI".length returned "3"
+ * "secret" + "agent" returned "secretagent"
+ * word = "hi"
+   word += "there" returned "hithere"
+ * test = "string"
+   test.reverse
+   test returned "string"
+   reversed_str = test.reverse returned "gnirts"	
+ * test2 = "another string"
+   test2.reverse!
+   test2 returned "gnirts rehtona" (This is considered a dangerous thing to do)
+=end
