3b3f471b317c0d706cf65e472757e5f2b64a33c2
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-05 18:43:27 -0700

    Provides definitions that needed to be researched.

---
+++ ruby/secret_agents_sea_lions.rb
@@ -0,0 +1,13 @@
+=begin
+ -This is for Release 0
+ * Scope means area in which the variables assigned in them are known and/or visible. 
+ * Local Variable is a variable whose name begins with a lowercase letter (a-z) or underscore (_) is a local variable or method invocation.
+ * A local variable is only accessible from within the block of its initialization. 
+ * Global Variable is a variable whose name begins with '$' has a global scope; meaning it can be accessed from anywhere within the program during runtime.
+ * Implicit Return is when Ruby returns the last value evaluated.
+ * Side Effects of a method are any observable change caused by calling a function. 
+   However, a Return Value will allow one to break out of a method immediately.
+ * If a method is called after being written inside another method, an error message will occur.
+ =end
+
+
