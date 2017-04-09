04a9917d0ebf086e19b1c9842327d4afadcd23b7
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-09 16:12:42 -0700

    Adds pseudocode for client information

---
+++ ruby/hashes_sea_lions.rb
@@ -0,0 +1,13 @@
+=begin
+PSEUDOCODE FOR THE CODE:
+-The hash should have the keys set as symbols to avoid strings and =>.
+-The hash should have the values set to "".
+PSEUDOCODE FOR THE USER INTERFACE (DRIVER CODE):
+-The user will be asked questions that will fill out each value.
+-Some answers should be number-specific.
+-After each question is answered, the hash will be printed out.
+-Ask the user which key(s) needs to be updated.
+-If the user selects a key, give the user a chance to update it.
+-Ask the user for further updates.
+-If the user doesn't need an update, exit the program
+=end
