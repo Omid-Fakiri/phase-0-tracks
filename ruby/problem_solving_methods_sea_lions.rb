52995b0c1a653dcabebb31c2f286ed2d6f425e0b
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-09 20:01:35 -0700

    Calculates fibonacci numbers

--- ruby/problem_solving_methods_sea_lions.rb
+++ ruby/problem_solving_methods_sea_lions.rb
@@ -23,3 +23,19 @@ array = [5,10,15,20,25]
 p search_array(array,25)
 p search_array(array,10)
 p search_array(array,3)
+
+#This is for Release 1
+
+def fib(x)
+  array = [0]
+  (1..x-1).each do |num|
+    if num == 1
+      array.push 1
+    elsif num
+      array.push(array[num-1] + array[num-2])
+    end
+  end
+  array
+end
+
+p fib(100)
