0892f30e314d2a2b2f75ec66fc4462bcd3e5ed92
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-09 19:03:20 -0700

    Pseudocodes the nested data structure.

---
+++ ruby/nested_data_structures_sea_lions.rb
@@ -0,0 +1,7 @@
+=begin
+	This will be an attempt to breakdown the U.S. Executive Branch.
+	This branch of government contains: 1) U.S. President 2) U.S. Vice President
+	3) Cabinet.
+	This nested data structure will break down each part of the branch by:
+	name, position, and gender.
+=end
