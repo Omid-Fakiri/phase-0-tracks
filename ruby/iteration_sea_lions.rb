8ed2127d91d3d779678f910d781e7b9bf9f53ea2
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-09 18:42:02 -0700

    Creates array, hash. Iterates them properly.

--- ruby/iteration_sea_lions.rb
+++ ruby/iteration_sea_lions.rb
@@ -1,4 +1,5 @@
-#This is for Release 0
+=begin
+This is for Release 0
 def numbers
     n1 = 2
     n2 = 5
@@ -9,3 +10,35 @@ def numbers
 end
 
 numbers { |a, b, c| puts (a + b + c) }
+=end
+
+#This is for Release 1
+#array:
+favorite_movies = ["Before Sunset", "Roman Holiday", "Whiplash"]
+
+#hash:
+favorite_movies_ratings = {"Before Sunset" => 9, "Roman Holiday" => 8, "Whiplash" => 8.5}
+
+p favorite_movies
+
+favorite_movies.each do |film|
+	puts film.upcase
+end
+
+p favorite_movies_ratings
+
+favorite_movies_ratings.each do |film, rating|
+	puts film.upcase
+end
+
+p favorite_movies_ratings
+
+p favorite_movies
+
+p favorite_movies
+
+favorite_movies.map! do |film|
+	film.upcase
+end
+
+p favorite_movies
