ab5a55ca9babfa6c9217e714f1670c70d42f63aa
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-11 21:04:13 -0700

    Adds gender & ethnicity to initialize, reindeer & age methods, and each loop

--- ruby/santa_sea_lions.rb
+++ ruby/santa_sea_lions.rb
@@ -1,4 +1,5 @@
-#This is for Release 0
+=begin
+This is for Release 0
 class Santa
 	def initialize
 		puts "Initializing Santa instance ..."
@@ -17,3 +18,38 @@ santa = Santa.new
 
 santa.speak
 santa.eat_milk_and_cookies("chocolate chip")
+=end
+
+#This is for Release 1
+class Santa
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		puts "Initializing Santa instance ..."
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie} cookie!"
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end
+
+	def age
+		@age = 0
+	end
+end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
