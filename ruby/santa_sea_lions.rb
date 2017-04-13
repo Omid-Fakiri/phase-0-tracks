42d905f1d6f837775a772ac147bacd6c627dc6df
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-13 14:44:44 -0700

    Updates while loop to include speak and cookie methods, adds cookie array to driver code

--- ruby/santa_sea_lions.rb
+++ ruby/santa_sea_lions.rb
@@ -34,11 +34,11 @@ class Santa
 	end
 
 	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
+		p "Ho, ho, ho! Haaaappy holidays!"
 	end
 
 	def eat_milk_and_cookies(cookie)
-		puts "That was a good #{cookie} cookie!"
+		p "That was a good #{cookie} Cookie!"
 	end
 
 	def celebrate_birthday(age)
@@ -55,6 +55,7 @@ end
 genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+cookies = ["Chocolate Chip", "Chocolate", "Oatmeal Chocolate Chip", "Peanut Butter", "Snickerdoodle", "Sugar", "Ginger", "Oatmeal Raisin", "Macaroon", "Peanut Butter Blossom", "White Chocolate Macadamia Nut"]
 
 
 index = 0
@@ -64,11 +65,22 @@ santa = Santa.new(genders.sample, ethnicities.sample)
 santas = []
 santas << santa
 	santas.each do
+			puts "="*90
 			puts "This Santa is #{santa.age} years old."
+			puts "="*90
 			puts "This Santa is identifies as: #{santa.gender}, #{santa.ethnicity}."
+			puts "="*90
+			puts "This Santa says:"
+			santa.speak
+			puts "="*90
 			puts "This is the reindeer ranking for this Santa:"
 			p santa.get_mad_at(reindeer.sample)
+			puts "="*90
+			puts "This Santa now says:"
+			santa.eat_milk_and_cookies(cookies.sample)
+			puts "="*90
 			puts "This Santa is now #{santa.celebrate_birthday(@age)} years old after the birthday."
+			puts "="*90
 	end
 	index += 1
 end
