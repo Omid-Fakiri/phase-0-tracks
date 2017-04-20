4dcd0c4dd821d870995a0d61a4e6e2064b724f31
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-19 17:25:02 -0700

    Compares Variables, Data Types, and Printing between JavaScript and Ruby.

---
+++ js/explore_sea_lions.rb
@@ -0,0 +1,15 @@
+=begin
+-This is for Release 0
+
+1. In JavaScript, "null" is the equivalent to "nil" in Ruby.
+2. It is a good idea to use var, simply to avoid making the variable a global one, making it susceptable to being equal to something different in 
+the program.
+As far as I know right now, in Ruby, you can use signs like @ or $, to label the variables as instance and global, respectfully.  
+Right now, I don't know if there are such options in JavaScript.
+3. In JavaScript, you use "console.log()" and add the "+" in front of the variable.
+4. In JavaScript, you use ";" to end a line of code.
+5. In JavaScript, you use "//" to write comments. But, you can use "/*" to start a multi-line comment section, and end it with the reverse
+of the characters. If I do it now, I will lose the comment capability here, and the aesthetics will not be as pleasing even if I make the necesssary 
+fixes.
+=end
+
