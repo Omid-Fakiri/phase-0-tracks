239e47b586db9db878d7e92f64fed2856e7471a6
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-22 16:52:51 -0700

    Adds pseudocode file, cleans up game file

--- ruby/game_sea_lions/game_sea_lions.rb
+++ ruby/game_sea_lions/game_sea_lions.rb
@@ -1,17 +1,5 @@
-=begin
-PSEUDOCODE:
--The game will ask the first user to input one word
--The game will have one method take the letters of the word, and replace them with "_" characters
--The game will have one method that will provide the user with x amount of guesses. x = "_" * 2
--The game will have one method that will replace the "_" with each correct letter guess.
--The game will have one method that will determine if the guessing user has won or not.
--The game will have a driver code that will keep the game going WHILE the user is within the guess limits 
-AND hasn't fully guessed the word yet.
-=end
-
 class Game
 	attr_reader :guesses, :hidden_word, :game_over
-	#attr_accessor 
 
 	def initialize(word)
 		@word = word
@@ -22,7 +10,6 @@ class Game
 	end
 
 	def play(letter)
-		#update(letter)
 		count
 		guessed_letters(letter)
 		game_done
@@ -35,28 +22,12 @@ class Game
 			@guess_count = "guesses"
 		end
 	end
-#the user takes a guess. 
-#the program checks to see if the user has taken the guess before.
-	#If the user has taken the guess before
-		#tell the user that they have taken the guess before
-		#repeat to step 1(line 89)
-	#if the user has not taken the guess before
-		#record the guess as a guessed letter
-		#determine if the letter is a correct guess.
-			#if it is a correct guess
-				#update the space where the letter belongs
-				#repeat to step 1
-			#if the guess is incorrect
-				#inform the user he is wrong
-				#repeat to step 1
+
 	def guessed_letters(letter)
 		if @used_letters.include?(letter)
-			#return true 
 			puts "You have already guessed that letter. Try again!"
 			puts "You still have #{@guesses} #{count} left."
 		else
-			#return false
-		  #@used_letters << letter
 		  update(letter)
 		end
 	end
@@ -80,12 +51,10 @@ class Game
 				puts "You now have #{@guesses} #{count} left."
 			end				
 			puts @hidden_word.join(' ')
-			#guessed_letters(letter)
 	end
 
 	def game_done
 		if @guesses == 0 && @hidden_word.include?("_")
-			#puts "Sorry, you lose! Better luck next time"
 			puts "Sorry, you lose!"
 			puts "The word was: #{@word}"
 			puts "Better luck next time."
@@ -124,53 +93,5 @@ letter = gets.chomp
 while !game.game_over
 	game.play(letter)
 	puts "Enter another letter:"
-	#puts game.hidden_word.join(' ')	
 	letter = gets.chomp
 end
-
-
-#the user takes a guess. 
-#the program checks to see if the user has taken the guess before.
-	#If the user has taken the guess before
-		#tell the user that they have taken the guess before
-		#repeat to step 1(line 89)
-	#if the user has not taken the guess before
-		#record the guess as a guessed letter
-		#determine if the letter is a correct guess.
-			#if it is a correct guess
-				#update the space where the letter belongs
-				#repeat to step 1
-			#if the guess is incorrect
-				#inform the user he is wrong
-				#repeat to step 1
-
-	# def guessed_letters(letter)
-	# 	if @used_letters.include?(letter)
-	# 		puts "You've already used that letter. Try again!"
-	# 		puts "You have #{@guesses} guesses left."
-	# 	else
-	# 		@used_letters << letter
-	# 		@guesses -= 1
-	# 	  puts "Wrong guess! Try again!"
-	# 		puts "You have #{@guesses} guesses left."
-	# 	end
-	# 	letter_count = 0
-	# 	letter_index = 0
-	# 	@hidden_word.each do |l|
-	# 		if @word == l
-	# 			@hidden_word[letter_index] = l
-	# 			letter_count += 1
-	# 		end
-	# 		letter_index += 1
-	# 	end
-	# 		puts "You are making progress"
-	# 		puts "You have #{@guesses} guesses left."
-	# 		@used_letters << letter
- #  end
-
-
-
-
-
-
-
