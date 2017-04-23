d69a55cbc73f1c1fcc440ccb813f37f399d951eb
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-22 19:30:17 -0700

    Provides pseudocode for finding the longest phrase

---
+++ js/algos_sea_lions.js
@@ -0,0 +1,6 @@
+/*This is for Release 0
+PSEUDOCODE:
+-Create a variable called "array", and place the 3 phrases in the example inside the bracket.
+-Create an if/else if/else statement to return the array that has the highest .length value.
+-Place the driver code (arrays) above the code in order to test the functionality of the code.
+*/
