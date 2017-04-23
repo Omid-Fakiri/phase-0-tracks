a646d2fef7032b1f79ced3742a12226b32f1af64
Omid-Fakiri <ofakiri@hotmail.com>
2017-04-22 19:41:49 -0700

    Provides code for longest phrase

--- js/algos_sea_lions.js
+++ js/algos_sea_lions.js
@@ -4,3 +4,19 @@ PSEUDOCODE:
 -Create an if/else if/else statement to return the array that has the highest .length value.
 -Place the driver code (arrays) above the code in order to test the functionality of the code.
 */
+
+var array = ["long phrase","longest phrase","longer phrase"]; //returned "longest phrase" (On index 1)
+var array = ["Warriors", "49ers", "Athletics"]; //returned "Athletics" (On index 2)
+var array = ["Web Development", "Live TV", "Small Gigs"]; //returned "Web Development" (On index 3)
+
+function calculate(array) {
+  if (array[0].length > array[1].length && array[0].length > array[2].length) {
+  console.log(array[0]);
+} else if (array[1].length > array[0].length && array[1].length > array[2].length) {
+  console.log(array[1]);
+} else {
+  console.log(array[2]);
+}  
+}
+
+calculate(array);
